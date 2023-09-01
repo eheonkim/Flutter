f533c2a214ab218d40d5dd33e67f7bf2c17889e4
DESKTOP-25TV5SJ\study02 <dlgjs414@daum.net>
2023-09-01 17:49:13 +0900

    기본위젯 01~04까지

---
+++ ex01_start2/android/app/src/main/kotlin/com/study/ex01_start2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.study.ex01_start2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ex4_icon/android/app/src/main/kotlin/com/study/ex4_icon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.study.ex4_icon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
